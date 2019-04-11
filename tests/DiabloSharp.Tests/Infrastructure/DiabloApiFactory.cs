ddb85baf801c992e28c332ba5b93f0341eb78b56
Lennart Pfeiffer <lennart.pfeiffer@leardev.de>
2019-04-11 22:33:16 +0200

    #6 user secrets for api credentials
    implement user secrets for clientId and clientSecret
    move tests

--- tests/DiabloSharp.Tests/Infrastructure/DiabloApiFactory.cs
+++ tests/DiabloSharp.Tests/Infrastructure/DiabloApiFactory.cs
@@ -1,20 +1,21 @@
 ﻿using DiabloSharp.Configurations;
 using DiabloSharp.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace DiabloSharp.Tests.Infrastructure
 {
     internal class DiabloApiFactory
     {
-        private const string ClientId = "";
-
-        private const string ClientSecret = "";
-
         public static IDiabloApi CreateApi()
         {
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets<BattleNetApiCredentials>();
+            var secretsConfiguration = builder.Build();
+
             var configuration = new DiabloApiConfiguration
             {
-                ClientId = ClientId,
-                ClientSecret = ClientSecret,
+                ClientId = secretsConfiguration["BattleNetApiCredentials:ClientId"],
+                ClientSecret = secretsConfiguration["BattleNetApiCredentials:ClientSecret"],
                 Region = Region.Europe,
                 Localization = Localization.EnglishUs
             };
