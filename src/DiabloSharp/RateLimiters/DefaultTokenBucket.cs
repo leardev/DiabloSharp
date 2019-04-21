be28466bf199d2cd85e53d89ebb5f5c0792d6f83
Malte Thomßen <malte.thomssen@leardev.de>
2019-04-21 15:36:54 +0200

    #35 support rate limiting
    o add default implementation of TokenBucket (respecting rate limiting from blizz)

---
+++ src/DiabloSharp/RateLimiters/DefaultTokenBucket.cs
@@ -0,0 +1,12 @@
+using System;
+using DiabloSharp.Infrastructure;
+
+namespace DiabloSharp.RateLimiters
+{
+    internal class DefaultTokenBucket : TokenBucket
+    {
+        public DefaultTokenBucket() : base(100, 100, TimeSpan.FromSeconds(1), new DateTimeSystem())
+        {
+        }
+    }
+}
